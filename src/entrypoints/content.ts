f992bd9281545481d6861b76b871f9a64094ee3a
meuzi8122 <kagiakete1234@gmail.com>
2025-12-06 18:02:26 +0900

    * 不要行削除

--- src/entrypoints/content.ts
+++ src/entrypoints/content.ts
@@ -1,12 +1,10 @@
 export default defineContentScript({
   matches: ["https://www.youtube.com/watch?v=*"],
-  // runAt: "document_end",
   main() {
     new Promise((resolve) => setTimeout(resolve, 1000)).then(() => {
       const title = document.getElementsByClassName(
         "yt-video-attribute-view-model__title"
       )[0]?.textContent;
-      console.log(title);
     });
   },
 });
