10a6e10ed50806dd5ec448984cc506730b2955ca
meuzi8122 <kagiakete1234@gmail.com>
2025-12-06 22:19:47 +0900

    fix message type

--- src/entrypoints/background.ts
+++ src/entrypoints/background.ts
@@ -2,7 +2,7 @@ import { findTablatures } from "@/lib/songstter/find-tablatures";
 import { onMessage } from "@/lib/utils/messaging";
 
 export default defineBackground(() => {
-  onMessage("findTablatures", async (message) => {
+  onMessage("findTablatures", async (message: { data: string }) => {
     return await findTablatures(message.data);
   });
 });
