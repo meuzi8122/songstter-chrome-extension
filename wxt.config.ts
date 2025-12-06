9393acfbefef75454728a4162f88cfe2bab883a6
meuzi8122 <kagiakete1234@gmail.com>
2025-12-06 17:27:49 +0900

    add content-script config

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  srcDir: 'src',
-  modules: ['@wxt-dev/module-svelte'],
+  srcDir: "src",
+  modules: ["@wxt-dev/module-svelte"],
+  runner: {
+    // bun run devでブラウザを立ち上げた時に開くサイトのURL
+    startUrls: ["https://www.youtube.com/watch?v=l9-E-7kRJes"],
+  },
+  manifest: {
+    content_scripts: [
+      {
+        matches: ["https://www.youtube.com/watch?v=*"],
+        js: ["entrypoints/content.ts"],
+      },
+    ],
+  },
 });
