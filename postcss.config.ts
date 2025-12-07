201ae403f47f38e1a7d0c452de2018ae435b8876
meuzi8122 <kagiakete1234@gmail.com>
2025-12-07 19:49:29 +0900

    fix postcss config

--- postcss.config.ts
+++ postcss.config.ts
@@ -2,8 +2,28 @@ module.exports = {
   plugins: {
     "@tailwindcss/postcss": {},
     "postcss-prefix-selector": {
-      prefix: "#crxNameHere",
+      // id="songstter-chrome-extension"とその子要素にだけスタイルを適用する（ページ全体にスタイルが適用されるのを防ぐ）
+      prefix: "#songstter-chrome-extension",
+      transform: transformSelector,
     },
     autoprefixer: {},
   },
 };
+
+function transformSelector(
+  prefix: string,
+  selector: string,
+  prefixedSelector: string
+) {
+  // daisyUI: src/base/colors.css
+  if (selector.match(/:root/)) {
+    return selector.replace(/:root/, prefix);
+  }
+
+  // daisyUI: src/base/general.css
+  if (selector.match(/html/)) {
+    return selector.replace(/html/, prefix);
+  }
+
+  return prefixedSelector;
+}
--- src/entrypoints/popup/main.ts
+++ src/entrypoints/popup/main.ts
@@ -1,9 +1,9 @@
-import { mount } from 'svelte';
-import App from './App.svelte';
-import './app.css';
+import { mount } from "svelte";
+import App from "./App.svelte";
+import "@/lib/styles/globals.css";
 
 const app = mount(App, {
-  target: document.getElementById('app')!,
+  target: document.getElementById("songstter-chrome-extension")!,
 });
 
 export default app;
