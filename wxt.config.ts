62ed391737507e2753d1f9cf4268c78014b6eff1
meuzi8122 <kagiakete1234@gmail.com>
2025-12-13 19:47:50 +0900

    add dev profile

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,3 +1,4 @@
+import * as path from "path";
 import { defineConfig } from "wxt";
 import tailwindcss from "@tailwindcss/vite";
 
@@ -8,6 +9,8 @@ export default defineConfig({
   runner: {
     // bun run devでブラウザを立ち上げた時に開くサイトのURL
     startUrls: ["https://www.youtube.com/watch?v=Fve_lHIPa-I"],
+    // cookieを永続化するため開発環境用の永続的なchromeプロファイルを作成
+    chromiumArgs: [`--user-data-dir=${path.resolve(".dev-profile")}`],
   },
   manifest: {
     name: "Songstter Chrome Extension",
